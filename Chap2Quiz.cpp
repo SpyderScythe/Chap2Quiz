1779ff806eeb54168b8b26948b9a4822fae65d64
Mike Prest <mike.prest84@gmail.com>
2020-02-08 11:35:34 -0500

    completed question 1

--- Chap2Quiz.cpp
+++ Chap2Quiz.cpp
@@ -2,7 +2,25 @@
 
 #include <iostream>
 
+int readNumber();
+void writeAnswer();
+
+int readNumber()
+{
+    std::cout << "Enter an integer:";
+    int x{};
+    std::cin >> x;
+    return x;
+}
+
+void writeAnswer(int input)
+{
+    std::cout << "Your numbers added together are: " << input << ".\n";
+}
+
 int main()
 {
+    int answer{ readNumber() + readNumber() };
+    writeAnswer(answer);
     return 0;
 }
